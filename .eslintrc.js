f7e9cd4fde7650da6355c52604efed0248d9d480
Pascal Sthamer <sthamer.pascal@gmail.com>
2021-04-06 10:50:30 +0200

    chore: update developer tool configs
    Signed-off-by: Pascal Sthamer <sthamer.pascal@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,64 @@
-module.exports = {
-  extends: [
-    'plugin:security/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
+const tsRules = {
+  // TypeScript already ensures we are not using non-existent variables.
+  'no-undef': 'off',
+  // This rule might interfere with prettier styling.
+  'no-unexpected-multiline': 'off',
+  // We need to access members of variables and assign to variables of type any (e.g. errors).
+  '@typescript-eslint/no-unsafe-member-access': 'off',
+  '@typescript-eslint/no-unsafe-assignment': 'off',
+  // Sometimes we need to call any, e.g. when a library does not have typings.
+  '@typescript-eslint/no-unsafe-call': 'off',
+  // Explicitly writing each functions return type feels cumbersome and is not very useful unless writing
+  // a public library api where it may be unsafe to infer return types.
+  '@typescript-eslint/explicit-function-return-type': 'off',
+  '@typescript-eslint/explicit-module-boundary-types': 'off',
+  // Explicit typing can help reading the code, whereas only relying on inferrable types can make code less readable.
+  '@typescript-eslint/no-inferrable-types': 'off',
+  // Template expressions are awesome for their ease of use. Not allowing booleans, numbers or undefined variables
+  // feels like a step back.
+  '@typescript-eslint/restrict-template-expressions': 'off',
+  // TypeScript sometimes can't know that a type is non-null (e.g. when using express middlewares).
+  // There are a lot of places in our code where that is the case and too many eslint-disable comments
+  // would harm code readability. non-null-assertions need to be used with care!
+  '@typescript-eslint/no-non-null-assertion': 'off',
+  // Allow rest args to be of type any (...args: any[]) and auto fix any usage to unknown.
+  '@typescript-eslint/no-explicit-any': [
+    'warn',
+    {
+      fixToUnknown: true,
+      ignoreRestArgs: true,
+    },
   ],
+}
+
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
   ignorePatterns: ['node_modules/', 'dist/'],
   root: true,
   env: {
-    es2017: true,
     node: true,
+    es2017: true,
   },
-  plugins: ['security', 'prettier'],
   rules: {
     'no-console': 'error',
     'no-bitwise': 'error',
-    'import/order': 'error',
-    // Allow unresolved imports. TypeScript already checks this and nuxt webpack aliases are not supported.
-    'import/no-unresolved': 'off',
-    'prettier/prettier': 'error',
-    'security/detect-object-injection': 'off',
   },
   overrides: [
     {
       files: ['*.ts'],
       extends: [
+        'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'prettier/@typescript-eslint',
+        'prettier', // We need to extend prettier again so that ts rules get disabled.
       ],
       plugins: ['@typescript-eslint'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        ecmaVersion: 2019,
+        ecmaVersion: 2020,
         sourceType: 'module',
         project: './tsconfig.json',
       },
-      rules: {
-        // We need to access members of variables and assign to variables of type any (e.g. errors).
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        // Sometimeis we need to call any, e.g. when a library does not have typings.
-        '@typescript-eslint/no-unsafe-call': 'off',
-        // Explicitly writing each functions return type feels cumbersome and is not very usefull unless writing
-        // a public library api where it may be unsafe to infer return types.
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        // Explicit typing can help reading the code, whereas only relying on inferrable types can make code less readable.
-        '@typescript-eslint/no-inferrable-types': 'off',
-        // Template expressions are awesome for their ease of use. Not allowing booleans, numbers or undefined variables
-        // feels like a step back.
-        '@typescript-eslint/restrict-template-expressions': 'off',
-        // Allow unused args.
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            vars: 'all',
-            // Unused function arguments often indicate a mistake in JavaScript code.  However in TypeScript code,
-            // the compiler catches most of those mistakes, and unused arguments are fairly common for type signatures
-            // that are overriding a base class method or implementing an interface.
-            args: 'none',
-          },
-        ],
-      },
+      rules: tsRules,
     },
   ],
 }
